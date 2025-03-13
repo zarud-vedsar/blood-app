daacaf498c9bd2827156ab91b1e85df7bc50b2ef
Shubham <shubhamkushwaha.vedsar@gmail.com>
2025-03-13 10:56:21 +0530

    f

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -1,6 +1,6 @@
 
 
-export const MAIN_URL = "http://localhost:3000/";
+export const MAIN_URL = "http://localhost:300/";
 export const NODE_API_URL = "http://localhost:9000";
 export const CKEDITOR_URL = "https://ghanshyamurvashipgcollege.com/cp-panel/assets/ckeditor/ckeditor.js";
 export const PHP_API_URL = "http://localhost/htdocs/blood-app/backend/php-server/controller";
