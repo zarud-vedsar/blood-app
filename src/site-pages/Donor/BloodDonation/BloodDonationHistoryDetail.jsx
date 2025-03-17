6860136a940893918be4c987321f0c1d8f7dfd89
Aman Yadav <amanyddv@gmail.com>
2025-03-17 17:28:35 +0530

    updated

--- src/site-pages/Donor/BloodDonation/BloodDonationHistoryDetail.jsx
+++ src/site-pages/Donor/BloodDonation/BloodDonationHistoryDetail.jsx
@@ -3,6 +3,7 @@ import React, { useState, lazy, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { PHP_API_URL } from "../../../site-components/Helper/Constant";
 import secureLocalStorage from "react-secure-storage";
+import { formatDate } from "../../../site-components/Helper/HelperFunction";
 const HeaderWithBack = lazy(() =>
   import("../../../site-components/Donor/components/HeaderWithBack")
 );
@@ -40,7 +41,7 @@ const BloodDonationHistoryDetail = () => {
         if (response?.data?.status === 200) {
           const data = response?.data?.data?.requestDetail;
           setBloodDonationRequestDetail(response?.data?.data[0]);
-          
+
           toast.error("An error occurred. Please try again.");
         }
       } catch (error) {
@@ -60,21 +61,19 @@ const BloodDonationHistoryDetail = () => {
     fetchData(); // Call the async function
   }, [id]); // Only runs when `id` changes
 
-  const handleSubmitRemark = async (status, historyid) => {
+  const handleSubmitRemark = async () => {
     setIsSubmit(true);
 
     try {
       const bformData = new FormData();
-      if (status === 0) {
-        bformData.append("data", "rejectDonation_requestor");
-      } else {
-        bformData.append("data", "confirmDonation");
-      }
-      bformData.append("remark", formData?.remark);
+      
+      bformData.append("data", "rejectDonation_donar");
+      
+      bformData.append("rejection_reason", formData?.remark);
       bformData.append("loguserid", formData?.loguserid);
-      bformData.append("id", bloodDonationRequestDetail?.id);
-      bformData.append("historyid", historyid);
-      bformData.append("type", status);
+      bformData.append("req_id", bloodDonationRequestDetail?.req_id);
+      bformData.append("historyid", id);
+      
 
       const response = await axios.post(`${PHP_API_URL}/doner.php`, bformData);
       if (response?.data?.status === 201 || response?.data?.status === 200) {
@@ -101,48 +100,11 @@ const BloodDonationHistoryDetail = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
-    if (name === "attendeePhone") {
-      if (!/^\d{0,10}$/.test(value)) {
-        return;
-      }
-    }
 
     setFormData({ ...formData, [name]: value });
   };
 
-  const deleteRequest = async (id) => {
-    setIsSubmit(true);
-    try {
-      const bformData = new FormData();
-      bformData.append("data", "deleteDonationReq");
-      bformData.append("loguserid", secureLocalStorage.getItem("loguserid"));
-      bformData.append("id", id);
-
-      const response = await axios.post(`${PHP_API_URL}/doner.php`, bformData);
-
-      setTimeout(() => {
-        window.location.reload();
-      }, 300);
-      if (response?.data?.status === 200) {
-        setTimeout(() => {
-          window.location.reload();
-        }, 300);
-      } else {
-        toast.error("An error occurred. Please try again.");
-      }
-    } catch (error) {
-      const status = error.response?.data?.status;
-      if (status === 400 || status === 500 || status === 401) {
-        toast.error(error.response.data.msg || "A server error occurred.");
-      } else {
-        toast.error(
-          "An error occurred. Please check your connection or try again."
-        );
-      }
-    } finally {
-      setIsSubmit(false);
-    }
-  };
+ 
 
   return (
     <>
@@ -225,7 +187,7 @@ const BloodDonationHistoryDetail = () => {
                 {bloodDonationRequestDetail?.city}
               </div>
             </div>
-            <div class="row mb-2">
+            <div class="row ">
               <div class="col-5">
                 <strong class="f-17 fw-700"> Address </strong>
               </div>
@@ -234,7 +196,39 @@ const BloodDonationHistoryDetail = () => {
                 {bloodDonationRequestDetail?.address}
               </div>
             </div>
-            <div class="row">
+            <div class="row ">
+              <div class="col-5">
+                <strong class="f-17 fw-700"> Accepted Date</strong>
+              </div>
+              <div class="col-1">:</div>
+              <div class="col-auto fw-16 fw-600">
+                {formatDate(bloodDonationRequestDetail?.acceptance_date)}
+              </div>
+            </div>
+            {bloodDonationRequestDetail?.approval_date && (
+              <div class="row ">
+                <div class="col-5">
+                  <strong class="f-17 fw-700"> Approved Date</strong>
+                </div>
+                <div class="col-1">:</div>
+                <div class="col-auto fw-16 fw-600 text-success">
+                  {formatDate(bloodDonationRequestDetail?.approval_date)}
+                </div>
+              </div>
+            )}
+            {bloodDonationRequestDetail?.rejection_date && (
+              <div class="row ">
+                <div class="col-5">
+                  <strong class="f-17 fw-700"> Rejected Date</strong>
+                </div>
+                <div class="col-1">:</div>
+                <div class="col-auto fw-16 fw-600 text-danger">
+                  {formatDate(bloodDonationRequestDetail?.rejection_date)}
+                </div>
+              </div>
+            )}
+
+            <div class="row mt-1">
               <div class="col-12">
                 <strong class="f-17 fw-700"> Additional Note </strong>
               </div>
@@ -243,183 +237,31 @@ const BloodDonationHistoryDetail = () => {
               </div>
             </div>
 
-            {bloodDonationRequestDetail?.doner &&
-              bloodDonationRequestDetail?.doner?.length > 0 && (
-                <>
-                  <div class="row mt-2">
-                    <div class="col-12">
-                      <strong class="f-17 fw-700">Donor </strong>
-                    </div>
-                  </div>
-                  {bloodDonationRequestDetail?.doner?.map((data, index) => (
-                    <div
-                      className="card mb-3"
-                      key={index}
-                      style={{ background: "#eaeaea" }}
-                    >
-                      <div className="card-body">
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> Name </strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">{data?.name}</div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> Gender </strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">
-                            {data?.gender}
-                          </div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> Phone </strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">{data?.phone}</div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> Email </strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">{data?.email}</div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> City</strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">{data?.city}</div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> PinCode</strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">
-                            {data?.pincode}
-                          </div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> State</strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">{data?.state}</div>
-                        </div>
-                        <div class="row ">
-                          <div class="col-4">
-                            <strong class="f-17 fw-700"> Address</strong>
-                          </div>
-                          <div class="col-1">:</div>
-                          <div class="col-auto fw-16 fw-600">
-                            {data?.address}
-                          </div>
-                        </div>
-
-                        {data?.rejection_reason ? (
-                          <>
-                            <div class="row">
-                              <div class="col-12">
-                                <strong class="f-17 fw-700"> Remark </strong>
-                              </div>
-                              <div class="col-auto fw-16 fw-600">
-                                {data?.rejection_reason}
-                              </div>
-                            </div>
-                          </>
-                        ) : (
-                          <>
-                            <div className="form-group basic">
-                              <label
-                                className="label f-17 fw-700"
-                                htmlFor="remark"
-                              >
-                                Remark :
-                              </label>
-                              <textarea
-                                className="form-control"
-                                name="remark"
-                                id="remark"
-                                placeholder="Enter remark"
-                                value={formData.remark}
-                                onChange={handleInputChange}
-                              />
-                            </div>
-                            <div className="d-flex gap-2">
-                              <button
-                                className="btn btn-danger btn-block"
-                                onClick={() =>
-                                  handleSubmitRemark(0, data?.historyid)
-                                }
-                              >
-                                {isSubmit ? (
-                                  "Submitting..."
-                                ) : (
-                                  <span className="fontsize-normal">
-                                    Reject
-                                  </span>
-                                )}
-                              </button>
-                              <button
-                                className="btn btn-success btn-block"
-                                onClick={() =>
-                                  handleSubmitRemark(1, data?.historyid)
-                                }
-                              >
-                                {isSubmit ? (
-                                  "Submitting..."
-                                ) : (
-                                  <span className="fontsize-normal">
-                                    Received
-                                  </span>
-                                )}
-                              </button>
-                            </div>
-                          </>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-                </>
-              )}
+            <div className="form-group basic mt-1">
+              <label className="label f-17 fw-700" htmlFor="remark">
+                Remark <span className="text-danger">*</span> :
+              </label>
+              <textarea
+                className="form-control"
+                name="remark"
+                id="remark"
+                placeholder="Enter remark"
+                value={formData.remark}
+                onChange={handleInputChange}
+              />
+            </div>
 
             <div class="form-button-group transparent d-flex  align-items-center">
-              {bloodDonationRequestDetail?.status === 0 ||
-              bloodDonationRequestDetail?.status === 3 ? (
-                <div class="d-flex justify-content-between  border py-1 px-2 rounded-pill w-95">
-                  <Link
-                    to={`/blood-donation-request/edit/${bloodDonationRequestDetail?.id}`}
-                  >
-                    <button className="btn btn-light edit-emp">
-                      <ion-icon name="create-outline"></ion-icon>
-                    </button>
-                  </Link>
-
-                  <button
-                    className="btn btn-light text-danger delete-spare"
-                    onClick={() =>
-                      deleteRequest(
-                        bloodDonationRequestDetail?.id
-                      )
-                    }
-                  >
-                    <ion-icon name="trash-outline"></ion-icon>
-                  </button>
-                </div>
-              ) : bloodDonationRequestDetail?.status === 1 ? (
-                <div className="bg-info d-block w-100 p-1 text-center">
-                  Accepted By Someone
-                </div>
-              ) : (
-                <div className="bg-success d-block w-100 p-1 text-center rounded">
-                  Approved
-                </div>
-              )}
+              <button
+                className="btn btn-danger btn-block"
+                onClick={() => handleSubmitRemark()}
+              >
+                {isSubmit ? (
+                  "Submitting..."
+                ) : (
+                  <span className="fontsize-normal">Reject</span>
+                )}
+              </button>
             </div>
           </div>
         </div>
