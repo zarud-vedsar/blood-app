65305c1c5ed43daca4b9e65f97a748b00a195160
Shubham <shubhamkushwaha.vedsar@gmail.com>
2025-03-17 15:18:40 +0530

    d

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -3,6 +3,6 @@
 export const MAIN_URL = "http://localhost:3000/";
 export const NODE_API_URL = "http://localhost:9000";
 export const CKEDITOR_URL = "https://ghanshyamurvashipgcollege.com/cp-panel/assets/ckeditor/ckeditor.js";
-export const PHP_API_URL = "http://localhost/htdoc/blood-app/backend/php-server/controller";
+export const PHP_API_URL = "http://localhost/htdocss/blood-app/backend/php-server/controller";
 export const FILE_API_URL = "http://localhost:9000/public/upload";
 export const PINCODE_URL = "https://api.postalpincode.in/pincode";
