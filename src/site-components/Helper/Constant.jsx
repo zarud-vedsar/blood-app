69446d0194f69b23f8925ab7d146de403892839b
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-04-04 18:58:39 +0530

    s

--- src/site-components/Helper/Constant.jsx
+++ src/site-components/Helper/Constant.jsx
@@ -1,5 +1,5 @@
 export const MAIN_URL = "http://localhost:3000/";
 export const NODE_API_URL = "http://localhost:9000";
-export const PHP_API_URL = "http://localhost/blood-app/backend/php-server/controller";
+export const PHP_API_URL = "http://localhost/donation-app/backend/php-server/controller";
 export const FILE_API_URL = "http://localhost:9000/public/upload";
 export const PINCODE_URL = "https://api.postalpincode.in/pincode";
